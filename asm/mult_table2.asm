8d730dafacaf6492673e3d348d2003e1485b8697
davey <davidralphhughes@college.harvard.edu>
2016-07-26 04:04:28 -0600

    Further golfs mult_table2
    Done some pretty crazy shit with the registers. Really bad practices,
    and it's probably pretty unstable, but code golf!

--- asm/mult_table2.asm
+++ asm/mult_table2.asm
@@ -1,5 +1,8 @@
 ; golfed version of mult_table
 ; doesn't exit properly and makes some assumptions about table size
+; also does crazy unstable stuff with registers
+
+; 76 bytes in machine code
 
 section .text
     global _start
@@ -7,50 +10,51 @@ section .text
 _start:
 
 _table:
-    mov     r12, 0x9        ; puts 9 into outside loop counter
+    mov     di, 0x9         ; sets outside loop counter to 9
+
 .L2:
     push    0xa
-    mov     r13, r12        ; sets inside loop counter to outside loop counter
+    mov     esi, edi        ; sets inside loop counter to outside loop counter
 .L3:
-    mov     rax, r13
-    mul     r12             ; multiplies the two numbers
+    mov     eax, esi
+    mul     edi             ; multiplies the two numbers
 
 _printInteger:              ; expects int to print in rax
     push    0x20            ; push space char onto the stack
+    cmp     al, 0xa
+    jge     .L1
+    push    0x20
 
 .L1:
-    xor     rdx, rdx        ; clear rdx
-    mov     r8, 0xa
-    div     r8              ; divide by 10
-    add     rdx, 0x30       ; convert to ascii
+    xor     dx, dx          ; clears out dx register
+    mov     bl, 0xa
+    div     bx
+    add     edx, 0x30       ; convert to ascii
     push    rdx             ; put char on stack
-    test    rax, rax        ; check if we've gone through the whole int
+    test    ax, ax          ; check if we've gone through the whole int
     jnz .L1
 
     push    0x3d            ; push '=' char onto stack
-    add     r12, 0x30       ; put multiplicand onto stack
-    push    r12
-    sub     r12, 0x30
+    push    di
+    add     byte [rsp], 0x30; converts to ascii on stack
     push    0x78            ; push 'x' char onto stack
-    add     r13, 0x30       ; put multiplier onto the stack
-    push    r13
-    sub     r13, 0x31       ; restores r13 and decrements by 1
+    push    si
+    add     byte [rsp], 0x30; converts to ascii on stack
 
-    test    r13, r13        ; loops if r13 hasn't reached 0
+    dec     esi             ; loops if esi hasn't reached 0 (sets ZF)
     jnz _table.L3
 
-    dec     r12
-    test    r12, r12        ; loops is r12 hasn't reached 0
+    dec     edi             ; loops is edi hasn't reached 0 (sets ZF)
     jnz _table.L2
 
 _printChars:
-    mov     rdx, 0x9b8
-    mov     rax, 0x1        ; syscall code for sys_write
-    mov     rdi, 0x1        ; write to stdout
-    lea     rsi, [rsp]      ; address of chars to write
+    mov     dx, 0x800
+    mov     al, 0x1         ; syscall code for sys_write
+    mov     di, 0x1         ; write to stdout
+    mov     rsi, rsp
     syscall
 
 ; _exit:                    ; proper exit not needed in code golf
     ; mov     rax, 0x3c
-    ; xor     rdi, rdi
+    ; xor     edi, edi
     ; syscall
