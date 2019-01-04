510173a68351e9afe4de11a3d177ba2cc40c875f
davey <davidralphhughes@college.harvard.edu>
2019-01-03 20:52:04 -0500

    Changes indentation in c files

--- div_test/div.c
+++ div_test/div.c
@@ -7,26 +7,26 @@
 #include <assert.h>
 
 unsigned divide(int a, int b) {
-    asm (
-        ".p2align 4\n"
-        "div%=:      cdq\n\t"
-        "   idiv    ecx\n"
-        "   test    edx, edx\n"
-        "   sete    al\n"
-        /* "   mov     eax, edx\n" */
-        "div_end%=:\n"
-        : "+a" (a), "+c" (b)
-        :
-        : "edx"
-    );
-    return a;
+	asm (
+		".p2align 4\n"
+		"div%=:		 cdq\n\t"
+		"	idiv	ecx\n"
+		"	test	edx, edx\n"
+		"	sete	al\n"
+		/* "   mov	   eax, edx\n" */
+		"div_end%=:\n"
+		: "+a" (a), "+c" (b)
+		:
+		: "edx"
+	);
+	return a;
 }
 
 int main(int argc, char** argv) {
-    assert(argc > 2);
-    int a = atoi(argv[1]), b = atoi(argv[2]);
+	assert(argc > 2);
+	int a = atoi(argv[1]), b = atoi(argv[2]);
 
-    printf(divide(a, b) ? "true\n" : "false\n");
+	printf(divide(a, b) ? "true\n" : "false\n");
 
-    return 0;
+	return 0;
 }
