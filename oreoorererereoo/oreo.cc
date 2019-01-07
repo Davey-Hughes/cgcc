37cb56b6a4c34714ce06d2149fa010652c840001
davey <davidralphhughes@college.harvard.edu>
2019-01-06 19:00:45 -0500

    Adds working asm solution

--- oreoorererereoo/oreo.cc
+++ oreoorererereoo/oreo.cc
@@ -5,41 +5,62 @@
 
 #include <string.h>
 
-extern "C" void oreo_asm(char *);
+extern "C" void oreo_asm(char *, char *);
 
 void
-oreo_c(char *input)
+oreo_c(std::string input)
 {
-	std::cout << input << std::endl;
+	int i = 0;
+	while (i < (int) input.length()) {
+		if (input[i] == 'o') {
+			std::cout << std::string(input.length(), 'o') << std::endl;
+			i++;
+		} else {
+			std::cout << ' ';
+			std::cout << std::string(input.length() - 2, 'e');
+			std::cout << ' ' << std::endl;
+			i += 2;
+		}
+	}
 }
 
 int
 main(int argc, char **argv)
 {
 	bool use_asm = false;
-	char *input = (char *) "";
+	std::string input("");
 	std::regex valid("^(o|re)+$");
 
 	for (int i = 0; i < argc; ++i) {
 		if (strncmp(argv[i], "--use_asm", 9) == 0) {
 			use_asm = true;
 		} else if (i != 0) {
-			input = argv[i];
+			input = std::string(argv[i]);
 		}
 	}
 
-	if (*input == '\0') {
+	if (input[0] == '\0') {
 		std::cout << "No input string specified" << std::endl;
 		return 1;
 	}
 
-	if (!std::regex_match(std::string(input), valid)) {
+	if (!std::regex_match(input, valid)) {
 		std::cout << "Input string invalid" << std::endl;
 		return 2;
 	}
 
 	if (use_asm) {
-		oreo_asm(input);
+		int max_len = input.length() * input.length() + input.length() + 1;
+		char *output = new char[max_len];
+
+		for (int i = 0; i < max_len; ++i) {
+			output[i] = 'x';
+		}
+
+		oreo_asm((char *) input.c_str(), output);
+		std::cout << output << std::endl;
+
+		delete[] output;
 	} else {
 		oreo_c(input);
 	}
--- oreoorererereoo/oreo.nasm
+++ oreoorererereoo/oreo.nasm
@@ -4,5 +4,75 @@ BITS 64
 section .text
 	global	oreo_asm
 
+; input passed through rdi
+; output passed through rsi
 oreo_asm:
+	push		rbx			; callee saved registers
+	push		r12
+	push		r13
+	push		r14
+	push		r15
+	push		rbp
+	mov		rbp, rsp
+
+	xor		rcx, rcx		; counter
+len:
+	inc		rcx
+	cmp byte	[rdi + rcx], 0x0
+	jne		len
+
+	mov		r12, rcx
+	mov		rax, r12		; r12 is len
+
+	mov		rcx, -0x1
+	mov		r13, 0x0		; string byte counter
+	mov		r14, rcx
+	jmp		outer_loop
+
+extra:
+	mov		r15, r13
+	sub		r15, 0x2
+	mov byte	[rsi + r15], 0x20
+	sub		r15, r12
+	inc		r15
+	mov byte	[rsi + r15], 0x20
+	dec		r14
+	jmp		outer_loop
+
+newline:
+	mov byte	[rsi + r13], 0xa	; put newline in output
+	inc		r13
+
+outer_loop:
+	inc		r14
+	inc		rcx
+	mov		r8b, [rdi + rcx]	; byte in input string
+	cmp		r8b, 0x65		; skip 'e' chars
+	je		extra
+
+	cmp		r8b, 0x0
+	je		done
+
+inner_loop:
+	mov		[rsi + r13], r8b
+	inc		r13
+	xor		rdx, rdx
+	mov		rax, r13
+	sub		rax, r14
+	mov		rbx, r12
+	div		rbx
+	cmp		rdx, 0x0
+	je		newline
+	jmp		inner_loop
+
+done:
+	mov byte	[rsi + r13], 0x0
+
+	mov		rsp, rbp		; restore callee saved registers
+	pop		rbp
+	pop		r15
+	pop		r14
+	pop		r13
+	pop		r12
+	pop		rbx
 	ret
